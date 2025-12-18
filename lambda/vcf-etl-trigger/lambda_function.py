fa7a4323e53b9ab7cbdbccbaaa6850f96eb95bfe
Robert Charewicz <ka.robert.charewicz@nhsbsa.nhs.uk>
2025-12-18 16:35:41 +0100

    s3 trigger removed, passing file via env variable for faster testing

--- lambda/vcf-etl-trigger/lambda_function.py
+++ lambda/vcf-etl-trigger/lambda_function.py
@@ -4,26 +4,23 @@ import os
 
 glue_client = boto3.client('glue')
 
-VCF_INPUT_PATH = os.environ.get('VCF_INPUT_PATH')
+VCF_INPUT_FILE = os.environ.get('VCF_INPUT_FILE')
 TABLE_BUCKET_ARN = os.environ.get('TABLE_BUCKET_ARN')
 GLUE_JOB_NAME = os.environ.get('GLUE_JOB_NAME')
 
 
 def lambda_handler(event, context):
     """
-    Lambda handler triggered by S3 upload of VCF files.
-    Starts Glue job to process the file.
+    Lambda handler that starts Glue job to process VCF file.
+    Uses VCF_INPUT_FILE environment variable for input file path.
     """
     try:
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        key = event['Records'][0]['s3']['object']['key']
-        
-        print(f"Processing: s3://{bucket}/{key}")
+        print(f"Processing: {VCF_INPUT_FILE}")
         
         response = glue_client.start_job_run(
             JobName=GLUE_JOB_NAME,
             Arguments={
-                "--VCF_INPUT_PATH": VCF_INPUT_PATH,
+                "--VCF_INPUT_FILE": VCF_INPUT_FILE,
                 "--TABLE_BUCKET_ARN": TABLE_BUCKET_ARN
             }
         )
@@ -32,6 +29,7 @@ def lambda_handler(event, context):
             'statusCode': 200,
             'body': json.dumps({
                 'message': 'Glue job started',
+                'input_file': VCF_INPUT_FILE,
                 'job_run_id': response['JobRunId']
             })
         }
